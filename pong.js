2f17e4061ff6c03bae78d8b4ef25a18186d7d075
Sean Bannon <seannbannon@gmail.com>
2022-04-07 12:23:31 -0500

    rectangle canvas is ther

--- pong.js
+++ pong.js
@@ -1,1 +1,16 @@
+// select canvas
 const canvas = document.getElementById("pong");
+const context = canvas.getContext("2d");
+
+// draw rectangle function
+function drawRectangle(x, y, w, h, color){
+    context.fillStyle = color;
+    context.fillRect(x, y, w, h);
+}
+
+drawRectangle(0, 0, canvas.width, canvas.height, "BLACK");
+
+//Draw Cricle
+function drawCircle(x, y, r, color){
+
+}
